41b4574087f731fcd5788877cc61d42561076284
misha1potapenko <misha1potapenko@yandex.ru>
2022-07-18 12:20:37 +0300

    added third test

--- thirdTest/Program.cs
+++ thirdTest/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает
+//  на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число: ");
+int NumberCube = Convert.ToInt32(Console.ReadLine());
+// double count = Math.Pow(NumberCube,3);
+// Console.WriteLine(count);
+void ForNumberCube(int Number)
+{
+    double count = 1;
+    double NumberCount = 1;
+    while (count <= Number)
+    {
+        NumberCount = Math.Pow(count,3);
+        Console.Write($"{NumberCount}, ");
+        count++;
+
+    }
+}
+
+ForNumberCube(NumberCube);
